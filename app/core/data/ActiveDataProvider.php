4fd3710d8742e9b1c96d87b7b30f28162383bc1d
Jesen <hanliu@gmail.com>
2016-10-27 13:47:41 +0800

    new

---
+++ app/core/data/ActiveDataProvider.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * YesCMS
+ * Copyright (c) 2016.
+ */
+
+namespace app\core\data;
+
+use Yii;
+use app\Models\User;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\db\ActiveRecord;
+use yii\base\Model;
+use core\components\BaseModel;
+use core\components\BaseModule;
+
+
+class ActiveDataProvider extends \yii\data\ActiveDataProvider
+{
+   
+}
